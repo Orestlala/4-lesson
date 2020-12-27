7a9808e9666d2f322c67c8190fe1e58b3de5f0e8
Orestvahila <orest.vahila@gmail.com>
2020-12-27 16:38:07 +0000

    1

--- JS/index.js
+++ JS/index.js
@@ -6,6 +6,12 @@ function updateTimer() {
     const endTime = new Date(2021, 01, 01, 00)
     const currTime = new Date()
     const diffSecs = Math.floor((endTime - currTime) / 1000);
-    document.querySelector(".timer").innerText = diffSecs;
+    const dayLeft = Math.floor(diffSecs / 60 / 60 / 24);
+    const hourLeft = Math.floor((diffSecs / 60 / 60) % 24);
+    const minuteLeft = Math.floor((diffSecs / 60) % 60);
+    const secsLeft = Math.floor(diffSecs % 60);
+
+
+    document.querySelector(".timer").innerText = dayLeft + ":" + hourLeft + ":" + minuteLeft + ":" + secsLeft;
 }
 setInterval(updateTimer, 1000)
