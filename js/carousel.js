504373006713dafa477305a27bde60932e69ddff
Orestlala <oresthuligan@gmail.com>
2020-12-30 16:18:41 +0000

    1

--- js/carousel.js
+++ js/carousel.js
@@ -13,7 +13,7 @@ function showCurrent () {
     const imgElement3 = document.querySelector('.carousel .img3');
     imgElement1.src = images[currentIdx];
     const idx2 = currentIdx + 1 >= images.length ? 0 : currentIdx +1;
-    const idx3 = idx2 + 1 > images.length ? 0 : idx2 + 1;
+    const idx3 = idx2 + 1 >= images.length ? 0 : idx2 + 1;
     imgElement1.src = images[currentIdx];
     imgElement2.src = images[idx2];
     imgElement3.src = images[idx3];
@@ -34,4 +34,5 @@ function prevIdx() {
 setInterval(nextIdx, 5000);
 //document.querySelector('.carousel .next').addEventListener('click', nextIdx);
 //document.querySelector('.carousel .prev').addEventListener('click', prevIdx);
+showCurrent();
 
